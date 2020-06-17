84143eca1861d412b74e6e353c26ed433164af65
Daniel Simão da Silva <simaodeveloper@gmail.com>
2020-06-17 15:42:29 -0300

    chore: add project to the repository

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  @Input() imageUrl = '';
+  @Input() title = '';
+  @Input() username = '';
+  @Input() content = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
